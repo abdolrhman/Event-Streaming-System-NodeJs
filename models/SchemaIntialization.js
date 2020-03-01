a1de33c4287bf04b7e91bc13b74ebcb315d52d83
abdolrhman <iwcw4i@gmail.com>
2020-03-01 13:42:37 +0200

    fix_some_issues_Add_addClickImple_AddPikeImpleAndConfiguration

--- models/SchemaIntialization.js
+++ models/SchemaIntialization.js
@@ -6,7 +6,7 @@ mongoose
   .connect(process.env.DATABASE_URL, { useNewUrlParser: true })
   .then(r => {});
 const db = mongoose.connection;
-
+mongoose.set("useFindAndModify", false);
 module.exports = mongoose;
 db.on("error", error => console.error(error));
 db.once("open", () => console.log("connected to database"));
